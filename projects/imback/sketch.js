6a0ad47ecfd7a2951cea9f02ddb8d7aa12e45761
strckture <33291729+strckture@users.noreply.github.com>
2021-07-20 17:13:28 +0200

    Update sketch.js

--- projects/imback/sketch.js
+++ projects/imback/sketch.js
@@ -5,9 +5,7 @@ const string = "I AM BACK"; //words to be displayed
 const showText = false; //whether or not to have an overlay of the original text (in the background color)
 const textAlpha = 30; //the alpha of the text if displayed (low value will make it slowly fade in)
 
-
 const fontSampleFactor = 0.5; //How many points there are: the higher the number, the closer together they are (more detail)
-
 const noiseZoom = 0.006; //how zoomed in the perlin noise is
 const noiseOctaves = 4; //The number of octaves for the noise
 const noiseFalloff = .5; //The falloff for the noise layers
@@ -16,7 +14,6 @@ const zOffsetChange = 0; //How much the noise field changes in the z direction e
 const individualZOffset = 0; //how far away the points/lines are from each other in the z noise axies (the bigger the number, the more chaotic)
 
 const lineSpeed = 1.5; //the maximum amount each point can move each frame
-
 const newPointsCount = 20; //the number of new points added when the mouse is dragged
 
 
@@ -30,15 +27,27 @@ function preload() {
 
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
+	background(244,236,221);
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(244,236,221);
+
+	var myColors = [color('#F90D0D'), color('#120DF9'), color('#3E3C87')];
+
+	for (var i = 0; i < myColors.length; i++) {
+		myColors[i].setAlpha(20);
+	}
+
+
 	textFont(font);
+
 	textSize(width/7);
 	fill(255, textAlpha);
-	stroke(255,0,0, 30);
+
+	stroke(myColors[int(random(0, myColors.length))]);
+
 	noiseDetail(noiseOctaves, noiseFalloff);
 
 	startingPoints = font.textToPoints(string, width / 2 - textWidth(string) / 2, height / 2, width/7, {"sampleFactor": fontSampleFactor});
@@ -54,7 +63,7 @@ function draw() {
 	if(showText){
 		noStroke();
 		text(string, width / 2 - textWidth(string) / 2, height);
-		stroke(255,0,0, 30);
+		stroke(255,0,1);
 	}
 	for (let pt = 0; pt < points.length; pt++) {
 		let p = points[pt];
