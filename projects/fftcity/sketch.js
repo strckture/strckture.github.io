6dff76bf8fc54541971865715b5b4ce90b2b98da
strckture <33291729+strckture@users.noreply.github.com>
2021-09-12 22:30:51 +0200

    test

--- projects/fftcity/sketch.js
+++ projects/fftcity/sketch.js
@@ -1,4 +1,3 @@
-
 let scl = 5;
 
 let cols;
@@ -9,27 +8,31 @@ let resp;
 
 const palette = ["#001219", "#005f73", "#0a9396", "#94d2bd", "#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226"];
 
+
 function preload(){
   sound = loadSound('assets/test.mp3');
 }
 
 
+
 function setup() {
   let cnv = createCanvas(windowWidth,windowHeight,WEBGL);
   cnv.mouseClicked(togglePlay);
+
   fft = new p5.FFT();
+
+
   background(0);
+
+
   ortho(-width / 2, width / 2, -height / 2, height / 2, -5000, 5000);
 
   seed = int(random(2,7));
-
-  resp = min(windowWidth,windowHeight);
-
   cols = min(windowWidth,windowHeight)/80;
   rows = min(windowWidth,windowHeight)/80;
 
-  colorMode(HSB,360,100,100);
 
+  colorMode(HSB,360,100,100);
 }
 
 
@@ -38,6 +41,7 @@ function draw() {
   background(202,63,88);
 
   let spectrum = fft.analyze();
+
 
   ambientLight(0, 0, 80);
 	directionalLight(color(30, 100, 80), 1, 0, -1);
@@ -49,14 +53,13 @@ function draw() {
   rotateX(-35);
 	rotateY(40);
 
-
   //Grid
   for (let i = 0; i< cols; i++) {
     for (let j = 0; j< rows; j++) {
       var x = 50*i;
       var z = 50*j;
 
-      let h = random(20,150)+ map(spectrum[i+j],0,255,0,200);
+      let h = random(20,150)+ map(spectrum[i+j],0,255,0,100);
 
       drawBuilding(x,h/2,z,int(random(20,50)),h);
     }
@@ -65,15 +68,15 @@ function draw() {
 }
 
 function drawBuilding(x,y,z,b,h) {
+  let c = palette[int(random(palette.length))];
+
   push();
   translate(x,y,z);
-  //drawWindows(b,h);
-
-  // push();
-  // rotateY(radians(90));
-  // drawWindows(b,h);
-  // pop();
-  let c = palette[int(random(palette.length))];
+  drawWindows(b,h);
+  push();
+  rotateY(radians(90));
+  drawWindows(b,h);
+  pop();
 
   fill(c);
   noStroke();
@@ -83,24 +86,41 @@ function drawBuilding(x,y,z,b,h) {
 
 function drawWindows(b,h) {
 
-  let wcols = b/15;
-  let wrows = h/14;
+  //let winHeight = 5;
+  let winHeight = h-20;
+  let winWidth = b/10;
+
+  let winRows = 3;
+  let winCols = h/winHeight-10;
+
+  // push()
+  // translate(b/-2, h/-2, b/2+1);
+  // for (let i = 0; i< winRows; i++) {
+  //   for (let j = 0; j< winCols; j++) {
+  //     var px = b/5*i;
+  //     var py = winHeight+5*j;
+  //     //push();
+  //     rectMode(CENTER)
+  //     //translate(px+b/-2,py+h/-2,b/2+1);
+  //     fill(70,10,100);
+  //     noStroke();
+  //     //rect(b/4,h/4,b/10,b/10);
+  //     rect(px+b/4,py+h/6,winWidth,winHeight);
+  //     //pop();
+  //   }
+  // }
+  // pop()
 
   push()
   translate(b/-2, h/-2, b/2+1);
-  for (let i = 0; i< wcols; i++) {
-    for (let j = 0; j< wrows; j++) {
-      var px = b/5*i;
-      var py = 10*j;
-      //push();
-      rectMode(CENTER)
-      //translate(px+b/-2,py+h/-2,b/2+1);
-      fill(70,10,100);
-      noStroke();
-      //rect(b/4,h/4,b/10,b/10);
-      rect(px+b/4,py+h/4,b/10,b/10);
-      //pop();
-    }
+  for (let i = 0; i< winRows; i++) {
+    var px = b/5*i;
+    var py = winHeight+10;
+    rectMode(CENTER)
+    fill(70,10,100,random(30));
+    //stroke(230,5,45,10);
+    noStroke();
+    rect(px+b/4,py-h/2+10,winWidth,winHeight);
   }
   pop()
 
